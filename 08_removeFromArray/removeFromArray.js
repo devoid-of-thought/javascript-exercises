18bace0c60e4bfb40bfdaaf3010367fc8d889355
devoid-of-thought <mikynowy@gmail.com>
2025-12-27 02:08:41 +0100

    Solutions to excercises up to javascript-excercises 12

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,arg1, ...args) {
+    const argsArray = [arg1, ...args];
+    return arr.filter(item => !argsArray.includes(item));
 };
 
 // Do not edit below this line
